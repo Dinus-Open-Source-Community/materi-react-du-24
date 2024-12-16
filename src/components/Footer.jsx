003623d1a17af95e9a57793f14f41cfedb680240
naufaladrian <naufal.adrian904@gmail.com>
2024-12-16 14:31:19 +0700

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-teal-600 text-center py-3 mt-10">
+    <div className="bg-amber-950 text-center py-3 mt-10">
       <h1>&copy; Doscom University</h1>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,83 @@
+import { X, Menu } from "lucide-react";
+import { useState } from "react";
+
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const navItems = [
+    {
+      label: "Home",
+      route: "#",
+    },
+    {
+      label: "Services",
+      route: "#",
+    },
+    {
+      label: "About",
+      route: "#",
+    },
+    {
+      label: "Gallery",
+      route: "#",
+    },
+  ];
+
+  const handleNavigate = () => {
+    setShowMenu(false);
+  };
+
   return (
-    <nav className="bg-blue-700 sm:bg-red-300 md:bg-green-300 lg:bg-gray-300 p-4">
-      <div className="max-7xl max-auto flex justify-between item-center">
+    <nav className="bg-amber-950 fixed top-0 w-full z-50">
+      <div className="max-7xl max-auto flex justify-between item-center p-4 shadow-xl">
         <div className="flex item-center">
           <h1 className="ml-4 text-white font-bold">Doscom</h1>
         </div>
-        <div className="flex space-x-4">
-          <a className="text-white hover:text-gray-400" href="#home">
-            Home
-          </a>
-          <a className="text-white hover:text-gray-400" href="#Home">
-            Services
-          </a>
-          <a className="text-white hover:text-gray-400" href="#Home">
-            About
-          </a>
-          <a className="text-white hover:text-gray-400" href="#Home">
-            Gallery
-          </a>
+        <div>
+          <div
+            className="md:hidden p-0 hover:border-2 rounded-md border-white cursor-pointer"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            {showMenu ? (
+              <X className="text-white" size={24} />
+            ) : (
+              <Menu className="text-white" size={24} />
+            )}
+          </div>
+          <ul className="hidden md:flex space-x-4">
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <a
+                  key={index}
+                  href={item.route}
+                  className="text-white hover:text-gray-400"
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
+      {showMenu && (
+        <ul className="md:hidden">
+          {navItems.map((item, index) => (
+            <li
+              key={index}
+              className="bg-yellow-900 p-4 py-2 hover:border-l-4 border-yellow-600"
+            >
+              <a
+                key={index}
+                href={item.route}
+                className="text-white hover:text-gray-400"
+                onClick={handleNavigate}
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </nav>
   );
 };
